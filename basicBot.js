fea50f9ce37ee639582dc87f8d41b7807afae70d
cringebot <32433016+cringebot@users.noreply.github.com>
2017-10-18 00:07:01 +0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -2575,7 +2575,7 @@
             },
 	 wootCommand: {
                 command: 'woot',
-                rank: 'residentdj',
+                rank: 'user',
                 type: 'exact',
                 functionality: function (chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
