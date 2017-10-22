73bf168ab59fb4c267291117cc0270114c2ebd38
cringebot <32433016+cringebot@users.noreply.github.com>
2017-10-23 02:51:13 +0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -319,9 +319,9 @@
             songstats: true,
             commandLiteral: '!',
             blacklists: {
-                NSFW: 'https://rawgit.com/basicBot/custom/master/blacklists/NSFWlist.json',
-                OP: 'https://rawgit.com/basicBot/custom/master/blacklists/OPlist.json',
-                BANNED: 'https://rawgit.com/basicBot/custom/master/blacklists/BANNEDlist.json'
+                NSFW: 'https://rawgit.com/cringebot/custom/master/blacklists/NSFWlist.json',
+                OP: 'https://rawgit.com/cringebot/custom/master/blacklists/OPlist.json',
+                BANNED: 'https://rawgit.com/cringebot/custom/master/blacklists/BANNEDlist.json'
             }
         },
         room: {
