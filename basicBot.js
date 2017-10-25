193d7fe925795eb8b3fd1bf75af86d3cfc0c521c
cringebot <32433016+cringebot@users.noreply.github.com>
2017-10-25 21:06:26 +0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -1885,7 +1885,7 @@
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void(0);
                     if (!basicBot.commands.executable(this.rank, chat)) return void(0);
                     else {
-                        API.sendChat(basicBot.chat.autowoot '+ more');
+                        API.sendChat('We recommend RCS for autowooting + more: http://rcs.radiant.dj/');
                     }
                 }
             },
