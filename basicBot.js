5c56d3928b1cb76e1ddfcf13b507df74aad02e9f
cringebot <32433016+cringebot@users.noreply.github.com>
2017-10-25 15:07:19 +0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -1865,6 +1865,20 @@
                 }
             },
 
+	   rcsCommand: {
+                command: 'rcs',
+                rank: 'user',
+                type: 'exact',
+                functionality: function(chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void(0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void(0);
+                    else {
+                        API.sendChat(basicBot.chat.autowoot);
+                    }
+                }
+            },
+
+		
             autowootCommand: {
                 command: 'autowoot',
                 rank: 'user',
