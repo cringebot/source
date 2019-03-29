a94fb785e562dbe1833610e4bdc37b0d3a6a2399
cringebot <32433016+cringebot@users.noreply.github.com>
2019-03-29 16:13:19 +0200

    Update basicbot.js

--- basicbot.js
+++ basicbot.js
@@ -247,7 +247,7 @@
             botName: 'BMN',
             language: 'english',
             chatLink: 'https://raw.githack.com/cringebot/source/master/lang/en.json',
-            scriptLink: 'https://raw.githack.com/cringebot/source/master/basicBot.js',
+            scriptLink: 'https://raw.githack.com/cringebot/source/master/basicbot.js',
             roomLock: false, // Requires an extension to re-load the script
             startupCap: 1, // 1-200
             startupVolume: 0, // 0-100
