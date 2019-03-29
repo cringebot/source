420782cec97ffdfe21c5107fb881c27dfc92091d
cringebot <32433016+cringebot@users.noreply.github.com>
2019-03-29 14:47:57 +0200

    Update basicbot.js

--- basicbot.js
+++ basicbot.js
@@ -1050,12 +1050,11 @@
                     };
                 }
             }
-
             var lastplay = obj.lastPlay;
             if (typeof lastplay === 'undefined') return;
             if (basicBot.settings.songstats) {
                 if (typeof basicBot.chat.songstatistics === 'undefined') {
-                    API.sendChat('/me ' + lastplay.media.author + ' - ' + lastplay.media.title + ': ' + lastplay.score.positive + ':thumbsup:' + lastplay.score.grabs + ':star:' + lastplay.score.negative + ':thumbsdown:')
+                    API.sendChat('/me ' + lastplay.media.author + ' - ' + lastplay.media.title + ': ' + lastplay.score.positive + 'W/' + lastplay.score.grabs + 'G/' + lastplay.score.negative + 'M.')
                 } else {
                     API.sendChat(subChat(basicBot.chat.songstatistics, {
                         artist: lastplay.media.author,
