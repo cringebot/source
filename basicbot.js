74f60f38420345108fa62e46fea00b95d1b71e51
cringebot <32433016+cringebot@users.noreply.github.com>
2019-04-03 22:09:45 +0300

    Update basicbot.js

--- basicbot.js
+++ basicbot.js
@@ -1770,7 +1770,24 @@
                     }
                 }
             },
-
+ 	  songstatsdebugCommand: {
+                command: 'songstatsdebug',
+                rank: 'bouncer',
+                type: 'startsWith',
+                functionality: function(chat, cmd) {
+                if (typeof basicBot.chat.songstatistics === 'undefined') {
+                    API.sendChat('/me ' + lastplay.media.author + ' - ' + lastplay.media.title + ': ' + lastplay.score.positive + 'W/' + lastplay.score.grabs + 'G/' + lastplay.score.negative + 'M.')
+                } else {
+                    API.sendChat(subChat(basicBot.chat.songstatistics, {
+                        artist: lastplay.media.author,
+                        title: lastplay.media.title,
+                        woots: lastplay.score.positive,
+                        grabs: lastplay.score.grabs,
+                        mehs: lastplay.score.negative
+                    }))
+                }
+	      }
+            },
             afktimeCommand: {
                 command: 'afktime',
                 rank: 'bouncer',
